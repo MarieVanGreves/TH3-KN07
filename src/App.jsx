a6de01d5b94b5b4dad8cd3dd2a1a6a050584378a
MarieVanGreves <keiiskyx@gmail.com>
2023-07-04 16:32:28 +0200

    Started navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.scss'
-import NavBar from "../src/components/NavBar/NavBar"
-import Footer from "../src/components/Footer/Footer"
+import NavBar from "../src/components/NavBar/NavBar.jsx"
+import Footer from "../src/components/Footer/Footer.jsx"
 
 function App() {
   return (
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,2 +1,20 @@
 import "../NavBar/NavBar.scss"
+import logo from "./../../assets/images/TH3_KN07_LOGO.png"
+import {Link} from "react-router-dom"
 
+function NavBar() {
+    return (
+        <>
+        <div className="generalNav">
+            <div className="divLogo">
+                <img className="logoNav" src={logo} alt="Logo en ASCII avec noté THE KNOT" />
+            </div>
+            <div className="linkRow">
+                
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default NavBar;
